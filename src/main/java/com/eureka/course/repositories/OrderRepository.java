edc3ec9ee0bd8a95080dce41d43a8920d553e513
Gabriel Capoia <gcapoia@gmail.com>
2022-02-24 18:12:30 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/eureka/course/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.eureka.course.repositories;
+
+import com.eureka.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}

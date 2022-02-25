fb031f7a82b1c4044fc03046638fa5916ef5d7c0
Gabriel Capoia <gcapoia@gmail.com>
2022-02-25 18:07:59 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/eureka/course/config/TestConfig.java
+++ src/main/java/com/eureka/course/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.eureka.course.config;
 
-import com.eureka.course.entities.Category;
-import com.eureka.course.entities.Order;
-import com.eureka.course.entities.Product;
-import com.eureka.course.entities.User;
+import com.eureka.course.entities.*;
 import com.eureka.course.entities.enums.OrderStatus;
-import com.eureka.course.repositories.CategoryRepository;
-import com.eureka.course.repositories.OrderRepository;
-import com.eureka.course.repositories.ProductRepository;
-import com.eureka.course.repositories.UserRepository;
+import com.eureka.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,8 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -69,5 +65,12 @@ public class TestConfig implements CommandLineRunner {
         p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
